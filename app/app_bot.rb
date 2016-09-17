f13bb3c6e385feb0be97582adc75c41f84d03b31
Danil Pismenny <danil@brandymint.ru>
2016-09-17 22:43:02 +0300

    deploy

--- app/app_bot.rb
+++ app/app_bot.rb
@@ -7,12 +7,6 @@ class AppBot < BotBase
   end
 
   def command_test
-    rm = Telegrammer::DataTypes::ReplyKeyboardMarkup.new(
-      keyboard: [['/Опубликовать']],
-      one_time_keyboard: false
-    )
-
-    client.send_message chat_id: message.chat.id, text: 'test', reply_markup: rm
   end
 
   def command_Опубликовать
@@ -30,7 +24,6 @@ class AppBot < BotBase
   end
 
   def command_debug
-    offer = session_storage.get_offer
     data = {
       offer: offer,
       state: session_storage.get_state,
@@ -41,32 +34,54 @@ class AppBot < BotBase
   end
 
   def state_new_offer_title
-    reply "Установлен заголовок: #{message.text}. Введите описание."
+    reply "Установлен заголовок: '#{message.text}'. Введите описание."
     session_storage.set_next_state
     session_storage.set_offer_attribute :title, message.text
   end
 
   def state_new_offer_desc
-    reply "Установлено описание: #{message.text}. Введите цену."
+    reply "Установлено описание: '#{message.text}'. Введите цену."
     session_storage.set_next_state
     session_storage.set_offer_attribute :description, message.text
   end
 
   def state_new_offer_price
     money = Monetize.parse message.text
-    reply "Установлена цена: #{money}. Введите теги через запятую"
+    reply "Установлена цена: '#{money}'. Введите теги через запятую"
     session_storage.set_next_state
     session_storage.set_offer_attribute :price, money.to_f
   end
 
   def state_new_offer_tags
     tags = message.text.split(',')
-    reply "Теги: #{tags.join(',')}"
     session_storage.set_next_state
     session_storage.set_offer_attribute :tags, tags.join(',')
   end
 
   def state_new_offer_publicate
     reply "Публикуем?"
+  end
+
+  def publicate?
+    rm = Telegrammer::DataTypes::ReplyKeyboardMarkup.new(
+      keyboard: [['/Опубликовать']],
+      one_time_keyboard: true
+    )
+
+    text = "Объявление: #{offer_text}. Публикуем?"
+    client.send_message chat_id: message.chat.id, text: text, reply_markup: rm
+  end
+
+  def offer_text
+    [
+      "Заголовок: #{offer['title']}",
+      "Описание: #{offer['descriiption']}",
+      "Цена: #{offer['price']}",
+      "Теги: #{offer['tags']}"
+    ].join("\n")
+  end
+
+  def offer
+    session_storage.get_offer
   end
 end
